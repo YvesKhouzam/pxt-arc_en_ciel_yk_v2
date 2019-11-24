a0cf4a78940ae1750fe41aa6fe028fdc4f5529e3
YvesKhouzam <58145982+YvesKhouzam@users.noreply.github.com>
2019-11-24 17:14:38 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 let position: Position = null
-let positionTemp: Position = null
+let positionTemp: Position = null // un autre commentaire!
 let j = 0 //Une variable utilisée plus bas!
 player.onChat("run", function () {
     agent.teleport(positions.create(10, -5, -12), CompassDirection.South)
